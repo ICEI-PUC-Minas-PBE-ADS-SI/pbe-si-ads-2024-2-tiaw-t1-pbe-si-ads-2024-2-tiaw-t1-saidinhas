de9503a249e6c96d6bc60b90d194dfdd7480bd89
SaraPasolini <saraalmeida3606@gmail.com>
2024-10-20 15:03:27 -0300

    codigo/db.json codigo/public/index.html codigo/public/assets/images/fazerlogin-removebg-preview.png codigo/public/assets/js/login.js codigo/public/assets/js/pagInicial.js codigo/public/assets/js/usua.js codigo/public/modulos/login/login.html codigo/public/modulos/login/paginicial.html codigo/public/modulos/login/Usuario.html

---
+++ codigo/public/assets/js/pagInicial.js
@@ -0,0 +1,11 @@
+const API_URL = 'http://localhost:3000/paginaIncial'
+const LOGAR_URL = "/modulos/login/paginicial";
+
+
+document.getElementById('btnTenhoumCNPJ').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
+
+document.getElementById('btnBuscarRestaurantes').addEventListener('click', function() {
+    window.location.href = 'Usuario.html';
+});
---
+++ codigo/public/assets/js/usua.js
@@ -0,0 +1,53 @@
+const API_URL = 'http://localhost:3000/usuario';
+const LOGINUSUARIO_URL = "/modulos/login/Usuario.html";
+
+
+
+const API_URL_USUARIO = 'http://localhost:3000/usuario';
+
+
+
+async function salvaUsuario(event) {
+    event.preventDefault(); 
+
+    
+    const novoUsuario = {
+        nome: document.getElementById('txt_nome_completo').value,
+        cpf: document.getElementById('txt_cpf').value,
+        email: document.getElementById('txt_email_usuario').value,
+        cep: document.getElementById('cep_usuario').value,
+        logradouro: document.getElementById('txt_logradouro_usuario').value,
+        bairro: document.getElementById('txt_bairro_usuario').value,
+        cidade: document.getElementById('txt_cidade_usuario').value,
+        estado: document.getElementById('txt_estado_usuario').value,
+        senha: document.getElementById('txt_senha_usuario').value
+    };
+
+    try {
+        
+        const response = await fetch(API_URL_USUARIO, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(novoUsuario)
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao salvar o usuario: ' + response.statusText);
+        }
+
+        const dadosSalvos = await response.json();
+        console.log('Usuario salvo com sucesso:', dadosSalvos);
+
+        
+        alert('Usuário cadastrado com sucesso!');
+
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao salvar os dados.');
+    }
+}
+
+
+document.getElementById('register-form').addEventListener('submit', salvaUsuario);
