62f87b8afe3a517ff4d7cef1fbf82fe02ee269bc
gabrielHS204 <gabrielhss1677@gmail.com>
2024-11-19 17:22:26 -0300

    ajustes nas promocoes

--- codigo/public/assets/js/promocao.js
+++ codigo/public/assets/js/promocao.js
@@ -32,15 +32,46 @@ function toggleCaixaDesconto(button) {
             const precoOriginal = parseFloat(precoSpan.textContent);
             const desconto = parseFloat(input.value);
             const precoComDesconto = precoOriginal - (precoOriginal * (desconto / 100));
-            descontoSpan.textContent = ` (${desconto}% off)`;
-            descontoSpan.style.color = 'red'; // Adiciona a cor vermelha ao desconto
+
+            descontoSpan.textContent = `${desconto}% off`;
+            descontoSpan.style.color = 'red';
             precoSpan.style.textDecoration = 'line-through';
-            precoSpan.nextElementSibling.textContent = ` R$ ${precoComDesconto.toFixed(2)}`;
+
+            let precoNovoSpan = cardBody.querySelector('.preco-novo');
+            if (!precoNovoSpan) {
+                precoNovoSpan = document.createElement('span');
+                precoNovoSpan.className = 'preco-novo';
+                precoSpan.parentElement.insertBefore(precoNovoSpan, precoSpan.nextSibling);
+            }
+            precoNovoSpan.textContent = ` R$ ${precoComDesconto.toFixed(2)}`;
+            precoNovoSpan.style.marginLeft = '10px';
         };
         cardBody.appendChild(input);
     }
 }
-// JavaScript para controlar o popup e adicionar novos cards
+
+function toggleCaixaPontos(button) {
+    const cardBody = button.parentElement;
+    let input = cardBody.querySelector('.input-pontos');
+    if (input) {
+        input.remove();
+
+    } else {
+        input = document.createElement('input');
+        input.type = 'number';
+        input.className = 'input-pontos';
+        input.placeholder = 'Digite os pontos';
+        input.oninput = function () {
+            let pontosSpan = cardBody.querySelector('.pontos');
+            if (!pontosSpan) {
+                pontosSpan = document.createElement('span');
+                pontosSpan.className = 'pontos';
+                cardBody.appendChild(pontosSpan);
+            } pontosSpan.textContent = `Pontos: ${input.value}`;
+        };
+        cardBody.appendChild(input);
+    }
+}
 function showPopup() {
     document.getElementById('popup').style.display = 'flex';
 }
@@ -53,10 +84,12 @@ function adicionarCard() {
     var titulo = document.getElementById('titulo').value;
     var urlImagem = document.getElementById('urlImagem').value;
     var preco = document.getElementById('preco').value;
+    var pontos = document.getElementById('ponto').value;
 
     console.log('Título:', titulo);
     console.log('URL da Imagem:', urlImagem);
     console.log('Preço:', preco);
+
 
     if (!titulo || !urlImagem || !preco) {
         alert('Preencha todos os campos!');
@@ -71,9 +104,11 @@ function adicionarCard() {
         <img class="card-img" src="${urlImagem}" alt="Imagem do Card">
         <div class="card-body">
             <h5 class="card-title">${titulo}</h5>
-            <strong>R$:</strong><span>${preco}</span> <span id="desconto"></span> <br>
+            <strong>R$:</strong><span id="preco">${preco}</span> <span id="desconto"></span> <br>
             <button class="btn primary" onclick="toggleCaixaNumerica(this)">Adicionar</button>
             <button class="btn secondary" onclick="toggleCaixaDesconto(this)">Desconto</button>
+            <br><strong>Pontos:</strong><span id="ponto"></span>
+            <button class="btn primary" onclick="toggleCaixaPontos(this)">Adicionar Pontos</button>
         </div>
     `;
 
