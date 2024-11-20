a6e982ccd86027af92971199aff984f7231458a6
Gabriel Henrique <gabrielhss1677@gmail.com>
2024-11-19 21:25:49 -0300

    promo

--- codigo/public/assets/js/promocao.js
+++ codigo/public/assets/js/promocao.js
@@ -55,7 +55,6 @@ function toggleCaixaPontos(button) {
     let input = cardBody.querySelector('.input-pontos');
     if (input) {
         input.remove();
-
     } else {
         input = document.createElement('input');
         input.type = 'number';
@@ -67,11 +66,13 @@ function toggleCaixaPontos(button) {
                 pontosSpan = document.createElement('span');
                 pontosSpan.className = 'pontos';
                 cardBody.appendChild(pontosSpan);
-            } pontosSpan.textContent = `Pontos: ${input.value}`;
+            } 
+            pontosSpan.textContent = `Pontos: ${input.value}`;
         };
         cardBody.appendChild(input);
     }
 }
+
 function showPopup() {
     document.getElementById('popup').style.display = 'flex';
 }
@@ -84,12 +85,10 @@ function adicionarCard() {
     var titulo = document.getElementById('titulo').value;
     var urlImagem = document.getElementById('urlImagem').value;
     var preco = document.getElementById('preco').value;
-    var pontos = document.getElementById('ponto').value;
 
     console.log('Título:', titulo);
     console.log('URL da Imagem:', urlImagem);
     console.log('Preço:', preco);
-
 
     if (!titulo || !urlImagem || !preco) {
         alert('Preencha todos os campos!');
@@ -105,9 +104,9 @@ function adicionarCard() {
         <div class="card-body">
             <h5 class="card-title">${titulo}</h5>
             <strong>R$:</strong><span id="preco">${preco}</span> <span id="desconto"></span> <br>
-            <button class="btn primary" onclick="toggleCaixaNumerica(this)">Adicionar</button>
+            <button class="btn primary" onclick="toggleCaixaNumerica(this)">Preço</button>
             <button class="btn secondary" onclick="toggleCaixaDesconto(this)">Desconto</button>
-            <br><strong>Pontos:</strong><span id="ponto"></span>
+            <br><strong>Pontos:</strong><span id="pontos"></span>
             <button class="btn primary" onclick="toggleCaixaPontos(this)">Adicionar Pontos</button>
         </div>
     `;
