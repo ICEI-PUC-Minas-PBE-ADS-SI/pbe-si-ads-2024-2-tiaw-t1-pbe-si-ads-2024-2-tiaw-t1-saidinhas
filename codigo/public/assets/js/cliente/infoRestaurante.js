4e775807f243cd9f2a240002c37388cc2a9e20ce
Yasmym-silva <yasmymsilva360@gmail.com>
2024-11-26 02:28:38 -0300

    confirmar

--- codigo/public/assets/js/cliente/infoRestaurante.js
+++ codigo/public/assets/js/cliente/infoRestaurante.js
@@ -100,3 +100,27 @@ function irParaCardapio() {
 }
 
 carregarPerfilRestaurante(restauranteId, criarPerfilRestaurante);
+
+
+function carregarDetalhesRestaurante(id) {
+    fetch(`http://localhost:3000/restaurantes/${id}`)
+        .then(response => response.json())
+        .then(restaurante => {
+            document.getElementById('restaurante-nome').textContent = restaurante.nome;
+            document.getElementById('restaurante-imagem').src = restaurante.image_url;
+            document.getElementById('restaurante-tipo').textContent = `Tipo: ${restaurante.tipo}`;
+            document.getElementById('restaurante-distancia').textContent = `Distância: ${restaurante.distancia}`;
+            document.getElementById('restaurante-avaliacao').textContent = `Avaliação: ${restaurante.avaliacao}`;
+            document.getElementById('restaurante-faixa-preco').textContent = `Faixa de preço: ${restaurante.faixa_preco}`;
+        })
+        .catch(error => console.error('Erro ao carregar os detalhes do restaurante:', error));
+}
+
+function irParaCardapio() {
+    window.location.href = `cardapio.html`;
+}
+
+document.getElementById('acessar-cardapio').addEventListener('click', irParaCardapio);
+
+carregarDetalhesRestaurante(restauranteId);
+
